4c3a0573d3fbbcfe6eba41ddfe5cc2be643e4c1d
Londou Jacques BOYODI <88946894+jackamrl@users.noreply.github.com>
2023-06-20 10:45:46 +0000

    learn slot

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,39 @@ export default {
       name: "Jack Dev",
       header: "Sign up for the Giveaway",
       text: "Tentez de gagnez les goodies DIDO en jouant le jeu",
+      showModal: false
     };
   },
+  methods: {
+    toogleModal() {
+      this.showModal = !this.showModal
+    }
+  }
+
 };
 </script>
 
 <template>
   <h1>{{ greet }} {{ name }}</h1>
-  <Modal :header="header" :text="text" theme="sale" />
+  <div v-if="showModal">
+    <Modal  theme="sale" @close="toogleModal" >
+      <template v-slot:links>
+        <a href="#">Sign up</a>
+        <a href="#">Sign in</a>
+      </template>
+        <h1>{{ header }}</h1>
+        <p>{{ text }}</p>
+
+      </Modal>
+  </div>
+  <button @click="toogleModal">Open modal</button>
 </template>
 
-<style ></style>
+<style >
+h1 {
+  font-size: 2rem;
+  font-weight: 700;
+  text-align: center;
+
+}
+</style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,54 +1,63 @@
 <template>
-    <div class="backdrop">
-        <div class="modal" :class="{ sale: theme === 'sale' }">
-            <h1>{{ header }}</h1>
-            <p>{{ text }}</p>
-        </div>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot></slot>
+
+      <div class="actions">
+        <slot name="links"> </slot>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ["header", "text", "theme"],
+  props: ["theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
 };
 </script>
 
 <style scooped lang="scss">
 .backdrop {
-    top: 0;
-    position: fixed;
-    background: rgba(0, 0, 0, 0.5);
-    width: 100%;
-    height: 100%;
+  top: 0;
+  left: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
 
-    .modal {
-        width: 500px;
-        padding: 20px;
-        margin: 100px auto;
-        background: #ffffff;
-        border-radius: 10px;
+  .modal {
+    width: 500px;
+    padding: 20px;
+    margin: 100px auto;
+    background: #ffffff;
+    border-radius: 10px;
 
-        h1 {
-            color: rgb(112, 13, 143);
-            border: none;
-            padding: 0;
-            font-weight: 700;
-            text-align: center;
-            margin: 10px;
-        }
-
-        p {
-            text-align: center;
-        }
+    h1 {
+      color: rgb(112, 13, 143);
+      border: none;
+      padding: 0;
+      font-weight: 700;
+      text-align: center;
+      margin: 10px;
     }
 
-    .modal.sale {
-        background: #ca3636;
-        color: #fff;
-
-        h1 {
-            color: #fff;
-        }
+    p {
+      text-align: center;
     }
+  }
+
+  .modal.sale {
+    background: #ca3636;
+    color: #fff;
+
+    h1 {
+      color: #fff;
+    }
+  }
 }
 </style>
